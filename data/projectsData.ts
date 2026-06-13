0bb2fd203262bd25a0d223016a806ec00421a8a2
UnknownBot <unknownbot951@gmail.com>
2026-06-13 13:02:36 +0530

    projects changed

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,10 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
+    title: 'Linear Algebra for Quantum I',
+    description: `Need linear algebra for quantum computing, this is one of the places, where you would find it!`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/blog/linear-algebra-for-quantum-I',
   },
 ]
 
